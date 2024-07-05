ea52437dcba90c47b7ce382fa3bbcd679ee52be5
fajri-farid <fajrijry14@gmail.com>
2024-07-06 06:52:48 +0800

    first commit

---
+++ bfs/bfs_graph-1.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> 
+
+#define N 4
+
+typedef struct {
+    int node; 
+    int jarak; 
+    int* jalur;  
+    int panjangJalur; 
+    bool dikunjungi[N]; 
+} jarakNode;
+
+void bfs(int graph[N][N], int start, int target) {
+    jarakNode* queue = NULL;
+    int front = 0, rear = 0, banyakAntrian = 0;
+    int jarakTerdekat = -1;
+
+    queue = (jarakNode*)realloc(queue, (banyakAntrian + 1) * sizeof(jarakNode));
+
+    jarakNode nodeAwal = {start, 0, NULL, 0, {false}};
+    nodeAwal.dikunjungi[start] = true;
+    queue[rear++] = nodeAwal;
+    banyakAntrian++;
+
+    while (front < rear) {
+        jarakNode sekarang = queue[front++];
+
+        // Jika menemukan target
+        if (sekarang.node == target) {
+            if (jarakTerdekat == -1) {
+                jarakTerdekat = sekarang.jarak;
+            }
+
+            if (sekarang.jarak > jarakTerdekat) {
+                break;
+            }
+
+            printf("Jalur terpendek dari simpul %d ke simpul %d: ", start, target);
+            for (int i = 0; i < sekarang.panjangJalur; i++) {
+                printf("%d ", sekarang.jalur[i]);
+            }
+            printf("%d\n", target);
+        }
+
+        // Memproses semua tetangga
+        for (int i = 0; i < N; i++) {
+            if (graph[sekarang.node][i] && !sekarang.dikunjungi[i]) {
+             
+                queue = (jarakNode*)realloc(queue, (banyakAntrian + 1) * sizeof(jarakNode));
+                int* jalurBaru = (int*)malloc((sekarang.panjangJalur + 1) * sizeof(int));
+
+                for (int j = 0; j < sekarang.panjangJalur; j++) {
+                    jalurBaru[j] = sekarang.jalur[j];
+                }
+                jalurBaru[sekarang.panjangJalur] = sekarang.node;
+
+                jarakNode nodeBaru = {i, sekarang.jarak + 1, jalurBaru, sekarang.panjangJalur + 1, {false}};
+                for (int j = 0; j < N; j++) {
+                    nodeBaru.dikunjungi[j] = sekarang.dikunjungi[j];
+                }
+                nodeBaru.dikunjungi[i] = true;
+
+                queue[rear++] = nodeBaru;
+                banyakAntrian++;
+            }
+        }
+
+        free(sekarang.jalur); 
+    }
+
+    free(queue); 
+}
+
+int main() {
+    int graph[N][N] = {
+        {0, 1, 1, 0},
+        {1, 0, 1, 1},
+        {1, 1, 0, 1},
+        {0, 1, 1, 0}
+    };
+    
+    bfs(graph, 0, 3); 
+    
+    return 0;
+}
---
+++ dfs/dfs_graph-1.c
@@ -0,0 +1,111 @@
+    #include <stdio.h>
+    #include <stdbool.h>
+    #include <stdlib.h>
+    #define N 4
+
+    typedef struct {
+        int node;       
+        int jarak;      
+        int* jalur;       
+        int panjangJalur;  
+        bool dikunjungi[N]; 
+    } jarakNode;
+
+    void dfs(int graph[N][N], int awal, int akhir, jarakNode* jalurMungkin, int* banyakJalurMungkin) {
+        jarakNode stack[100];
+        int ukuranStack = 0;
+        int jarakTerdekat = -1;
+
+        bool awalDikunjungi[N] = {false};
+        awalDikunjungi[awal] = true;
+        stack[ukuranStack++] = (jarakNode){awal, 0, NULL, 0, {false}};
+
+        while (ukuranStack > 0) {
+            jarakNode sekarang = stack[--ukuranStack];
+
+            if (sekarang.node == akhir) {
+                if (jarakTerdekat == -1 || sekarang.jarak <= jarakTerdekat) {
+                    jarakTerdekat = sekarang.jarak;
+
+                    jalurMungkin[*banyakJalurMungkin] = (jarakNode){
+                        .node = sekarang.node,
+                        .jarak = sekarang.jarak,
+                        .jalur = (int*)malloc(sekarang.panjangJalur * sizeof(int)),
+                        .panjangJalur = sekarang.panjangJalur
+                    };
+                    for (int i = 0; i < sekarang.panjangJalur; i++) {
+                        jalurMungkin[*banyakJalurMungkin].jalur[i] = sekarang.jalur[i];
+                    }
+                    (*banyakJalurMungkin)++;
+                }
+                continue;
+            }
+
+            for (int i = 0; i < N; i++) {
+                if (graph[sekarang.node][i] && !sekarang.dikunjungi[i]) {
+        
+                    int* jalurBaru = (int*)malloc((sekarang.panjangJalur + 1) * sizeof(int));
+
+                    for (int j = 0; j < sekarang.panjangJalur; j++) {
+                        jalurBaru[j] = sekarang.jalur[j];
+                    }
+                    jalurBaru[sekarang.panjangJalur] = sekarang.node;
+
+                    bool dikunjungiBaru[N];
+                    for (int j = 0; j < N; j++) {
+                        dikunjungiBaru[j] = sekarang.dikunjungi[j];
+                    }
+                    dikunjungiBaru[i] = true;
+        
+                    stack[ukuranStack++] = (jarakNode){i, sekarang.jarak + 1, jalurBaru, sekarang.panjangJalur + 1, {false}};
+                    for (int j = 0; j < N; j++) {
+                        stack[ukuranStack-1].dikunjungi[j] = dikunjungiBaru[j];
+                    }
+                }
+            }
+
+            free(sekarang.jalur); 
+        }
+    }
+
+    void printJalurTerpendek(jarakNode* jalurMungkin, int banyakJalurMungkin, int awal, int akhir) {
+        int jarakTerdekat = -1;
+
+        for (int i = 0; i < banyakJalurMungkin; i++) {
+            if (jarakTerdekat == -1 || jalurMungkin[i].jarak < jarakTerdekat) {
+                jarakTerdekat = jalurMungkin[i].jarak;
+            }
+        }
+
+        for (int i = 0; i < banyakJalurMungkin; i++) {
+            if (jalurMungkin[i].jarak == jarakTerdekat) {
+                printf("Jalur terpendek dari simpul %d ke simpul %d: ", awal, akhir);
+                for (int j = 0; j < jalurMungkin[i].panjangJalur; j++) {
+                    printf("%d ", jalurMungkin[i].jalur[j]);
+                }
+                printf("%d\n", akhir);
+            }
+        }
+    }
+
+    int main() {
+        int graph[N][N] = {
+            {0, 1, 1, 0},
+            {1, 0, 1, 1},
+            {1, 1, 0, 1},
+            {0, 1, 1, 0}
+        };
+
+        jarakNode jalurMungkin[100]; 
+        int banyakJalurMungkin = 0;       
+
+        dfs(graph, 0, 3, jalurMungkin, &banyakJalurMungkin);
+
+        printJalurTerpendek(jalurMungkin, banyakJalurMungkin, 0, 3); 
+
+        for (int i = 0; i < banyakJalurMungkin; i++) {
+            free(jalurMungkin[i].jalur);
+        }
+
+        return 0;
+    }
